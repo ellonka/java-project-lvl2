2dd74f5a9bb460841f451bddba269751508a7cc0
Ella Trofimova <ellatrofimova@MacBook-Pro-Ella.local>
2022-02-22 08:03:44 +0300

    displaying help

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,19 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
+import picocli.CommandLine.Command;
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", version = "1.0", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+
+   public static void main(String[] args) {
+        int exitCode = new picocli.CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+   }
+
+   @Override
+   public Integer call() throws Exception {
+      return 0;
+   }
 }
