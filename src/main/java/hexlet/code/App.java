f40d392fa7890b13e63fa8990845e6b0bafeff77
Ella Trofimova <ellatrofimova@MacBook-Pro-Ella.local>
2022-02-23 09:06:07 +0300

    add option -f and parameters in help

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,19 +1,30 @@
 package hexlet.code;
 
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", version = "1.0", mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
 
-   public static void main(String[] args) {
+    @Parameters(index = "0", description = "path to first file")
+    String filepath1;
+
+    @Parameters(index = "1", description = "path to second file")
+    String filepath2;
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    String format = "stylish";
+
+    @Override
+    public Integer call() throws Exception {
+        return 0;
+    }
+
+    public static void main(String[] args) {
         int exitCode = new picocli.CommandLine(new App()).execute(args);
         System.exit(exitCode);
-   }
-
-   @Override
-   public Integer call() throws Exception {
-      return 0;
-   }
+    }
 }
