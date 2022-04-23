3a3a1ba52815366ee5bbe680afd99fde03552ddf
ellonka <ella.trofimova2403@gmail.com>
2022-04-23 20:49:02 +0300

    fix amount of spaces

--- src/main/java/hexlet/code/formatter/Stylish.java
+++ src/main/java/hexlet/code/formatter/Stylish.java
@@ -10,11 +10,11 @@ public final class Stylish implements Format {
     @Override
     public void collect(String modification, String key, Object value1, Object value2) {
         switch (modification) {
-            case "ADDED" -> result.append(" + " + key + ": " + value2 + "\n");
-            case "DELETED" -> result.append(" - " + key + ": " + value1 + "\n");
-            case "UNCHANGED" -> result.append("   " + key + ": " + value1 + "\n");
-            case "UPDATED" -> result.append(" - " + key + ": " + value1 + "\n"
-                                + " + " + key + ": " + value2 + "\n");
+            case "ADDED" -> result.append("  + " + key + ": " + value2 + "\n");
+            case "DELETED" -> result.append("  - " + key + ": " + value1 + "\n");
+            case "UNCHANGED" -> result.append("    " + key + ": " + value1 + "\n");
+            case "UPDATED" -> result.append("  - " + key + ": " + value1 + "\n"
+                                + "  + " + key + ": " + value2 + "\n");
             default -> result.append("");
         }
     }
