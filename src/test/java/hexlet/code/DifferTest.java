864d8a86c4427bfcf3bc7bacdbfd82b371dd86ac
ellonka <ella.trofimova2403@gmail.com>
2022-04-24 10:21:19 +0300

    refactoring: delete unnecessary tests and files

--- src/test/java/hexlet/code/DifferTest.java
+++ src/test/java/hexlet/code/DifferTest.java
@@ -7,37 +7,6 @@ import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DifferTest {
-    @Test
-    public void testDifferBetweenTwoSimpleJsonWithStylish() throws IOException {
-        String pathToFile1 = "./src/test/resources/file1.json";
-        String pathToFile2 = "./src/test/resources/file2.json";
-
-        String expected = "{\n"
-                + "  - follow: false\n"
-                + "    host: hexlet.io\n"
-                + "  - proxy: 123.234.53.22\n"
-                + "  - timeout: 50\n"
-                + "  + timeout: 20\n"
-                + "  + verbose: true\n"
-                + "}";
-        assertEquals(expected, Differ.generate(pathToFile1, pathToFile2));
-    }
-
-    @Test
-    public void testDifferBetweenTwoSimpleYamlWithStylish() throws IOException {
-        String pathToFile1 = "./src/test/resources/file1.yml";
-        String pathToFile2 = "./src/test/resources/file2.yml";
-
-        String expected = "{\n"
-                + "  - follow: false\n"
-                + "    host: hexlet.io\n"
-                + "  - proxy: 123.234.53.22\n"
-                + "  - timeout: 50\n"
-                + "  + timeout: 20\n"
-                + "  + verbose: true\n"
-                + "}";
-        assertEquals(expected, Differ.generate(pathToFile1, pathToFile2));
-    }
 
     @Test
     public void testDifferBetweenTwoJsonWithNestedStructWithStylish() throws IOException {
